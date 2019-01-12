46426a018629fcf0cdbb0c47cf3ae719042c5484
juneandgreen <june_01@qq.com>
2019-01-12 19:23:48 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# karma-filemap-preprocessor
+# 简介
+
+将文件内容挂载到 `window.__FILE_MAP__` 之下。
+
+## 使用方式
+
+```
+npm install --save-dev karma-filemap-preprocessor
+```
+
+```
+// karma config
+module.exports = {
+    // ...
+    files: [
+      'src/**/*',
+      // ...
+    ],
+    preprocessors: {
+        "src/**/*": ['filemap'],
+        // ...
+    },
+    // ...
+};
+```
+
+## 协议
+
+MIT
---
+++ filemap.js
@@ -0,0 +1,16 @@
+const escapeContent = content => {
+  return content.replace(/\\/g, '\\\\').replace(/'/g, '\\\'').replace(/\r?\n/g, '\\n\' +\n    \'');
+};
+
+const createFileMapPreprocessor = logger => {
+  const log = logger.create('preprocessor.filemap');
+
+  return (content, file, done) => {
+    log.debug('Processing "%s".', file.originalPath);
+    done(`window.__FILE_MAP__ = window.__FILE_MAP__ || {};window.__FILE_MAP__[${file.originalPath}] = '${escapeContent(content)}'`);
+  };
+};
+
+createFileMapPreprocessor.$inject = ['logger'];
+
+module.exports = createFileMapPreprocessor;
